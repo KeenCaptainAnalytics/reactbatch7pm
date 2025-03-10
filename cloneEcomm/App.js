4ee81507824084876d9b286b77ccc7c7d5be72c2
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-10 20:00:55 +0530

    Create App.js

---
+++ cloneEcomm/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+
+import React from 'react'
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App

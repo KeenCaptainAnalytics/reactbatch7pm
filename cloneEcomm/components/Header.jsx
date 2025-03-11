a997c90929678d95d258c9108eceb7d581fc5202
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-11 19:56:23 +0530

    Update Header.jsx

--- cloneEcomm/components/Header.jsx
+++ cloneEcomm/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import "../styles/header.css"
+import Cart from './Cart'
+// import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+// import { PlusOne } from '@mui/icons-material';
 const Header = () => {
   return (
     <div className='header_container'>
             <ul>
                 <li>
+                    {/* <PlusOne/> */}
                     BuyMore
                     <div></div>
                 </li>
                 <li >
+                {/* <AccessAlarm/> */}
                     <span>37</span>
                     <div>
                         <h2>Order</h2>
@@ -19,7 +24,11 @@ const Header = () => {
                     <button class="active">Dashboard</button>
                     <button>Website</button>
                 </li>
-                <li>Cart</li>
+                <li style={{display:"flex", alignItems:"flex-end"}}>
+                    <Cart/>
+                <span>Cart</span>
+                    
+                </li>
             </ul>
     </div>
   )
